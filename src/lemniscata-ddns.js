929b27e8a22661f59479f801659fa0213ab81681
PauRealm <pauete.sb@gmail.com>
2026-07-09 01:18:13 +0200

    esto no iba PERO AHORA SÍ

--- src/lemniscata-ddns.js
+++ src/lemniscata-ddns.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const getCloudflareDnsData = async () => {
     try {
         return await fetch(`https://api.cloudflare.com/client/v4/zones/${process.env.ZONE_ID}/dns_records?name=${process.env.RECORD_NAME}&type=A`, {
@@ -40,15 +43,16 @@ const getWanIp = async () => {
 }
 
 const triggerIpUpdate = async currentWanIp => {
+    console.log("Triggering IP update")
     return new Promise(async (resolve) => {
         const dnsData = await getCloudflareDnsData().then(response => response.json())
-
         if (currentWanIp && dnsData && dnsData.result && dnsData.result.length == 1) {
             const recordData = dnsData.result[0];
+
             if (currentWanIp !== recordData.content) {
                 console.log(`Current address for ${process.env.RECORD_NAME} (${currentWanIp}) missmatch with DNS address (${recordData.content}). Requesting patch...`)
                 const patchResponse = await modifyRecordIp(recordData, currentWanIp).then(response => response.json())
-                console.log(patchResponse)
+                console.log("Success:", patchResponse.success)
             }
         }
 
@@ -57,6 +61,7 @@ const triggerIpUpdate = async currentWanIp => {
 }
 
 const listenToWanChanges = async () => {
+    console.log("Listening for WAN changes")
     let lastWanAddress = undefined
     let repeatsUntilFullComprobation = 1
     while (true) {
@@ -64,13 +69,18 @@ const listenToWanChanges = async () => {
             await new Promise(resolve => setTimeout(resolve, 1000 * process.env.PING_COOLDOWN))
 
             const currentWanIp = await getWanIp().then(response => response.text()).catch(error => lastWanAddress)
+            console.log("Current IP:", currentWanIp)
 
-            if (isValidIp(currentWanIp) ((currentWanIp !== lastWanAddress) || (repeatsUntilFullComprobation-- == 0))) {
+            if (isValidIp(currentWanIp) && ((currentWanIp !== lastWanAddress) || (repeatsUntilFullComprobation-- == 0))) {
+                console.log(`Updating IP from cloudflare (current: ${currentWanIp} - last: ${lastWanAddress})`)
+
                 await triggerIpUpdate(currentWanIp)
                 lastWanAddress = currentWanIp
                 repeatsUntilFullComprobation = 30
             }
-        } catch (_) {}
+        } catch (error) {
+            console.error(error)
+        }
         
     }
 }
@@ -79,4 +89,8 @@ const isValidIp = ip => {
     return ("" + ip).match(/^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/gm)
 }
 
+console.log("ZONE_ID:", process.env.ZONE_ID)
+console.log("RECORD_NAME:", process.env.RECORD_NAME)
+console.log("CLOUDFLARE_TOKEN:", (process.env.CLOUDFLARE_TOKEN).replace(/./g, "*"))
+console.log("PING_COOLDOWN:", process.env.PING_COOLDOWN)
 listenToWanChanges()
